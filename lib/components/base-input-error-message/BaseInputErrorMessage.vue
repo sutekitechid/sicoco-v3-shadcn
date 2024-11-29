57c5fc5271d982672282b8cbd06d142880195411
Anwar Ramadha <anwarramadha22@gmail.com>
2024-11-29 17:09:27 +0700

    Feat/Cu-86cx39drg__revamp-input (#9)
    * feat: :sparkles: added input
    
    * feat: :sparkles: added input
    
    * feat: :sparkles: added url validation

---
+++ lib/components/base-input-error-message/BaseInputErrorMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="invalid">
+    <slot />
+  </div>
+  <p
+    v-else
+    class="text-xs invisible"
+  >-</p>
+</template>
+
+<script setup lang="ts">
+/**
+ * Component that used as the base for input error message
+ */
+defineProps<{
+  invalid: boolean
+}>()
+</script>
---
+++ lib/components/input/InputMorpUnit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="morphUnitRef">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+/**
+ * Component that wraps the input prefix or suffix to get the width of the element
+ */
+import { computed, watch, ref } from 'vue'
+
+const morphUnitRef = ref(null)
+const emits = defineEmits()
+
+const morphUnitWidth = computed(() => {
+  const prefix = morphUnitRef.value
+  if (!prefix) return 0
+  return prefix.offsetWidth
+})
+
+watch(morphUnitWidth, (value) => {
+  emits('width-change', value)
+})
+</script>
