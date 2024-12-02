2d421c5a6a8633597a94c5357c4e450a550fa251
Maul <50138748+maulanairfanf@users.noreply.github.com>
2024-12-02 08:03:26 +0700

    Feat/cu 86cx39my9 shadcn badge (#8)
    * fix(Badge): :sparkles: CU-86cx39my9 add badge components
    
    * feat: :memo: CU-86cx39my9 add scope badge
    
    * feat(Badge): :white_check_mark: CU-86cx39my9 add. unit test
    
    * feat(Badge): :triangular_flag_on_post: CU-86cx4f029 remove data cy and test case
    
    * feat(Badge): :triangular_flag_on_post: CU-86cx4f029 remove unused color

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import './assets/index.css'
+import '../lib/assets/icomoon/style.css'
 import Button from '@/components/button/Button.vue'
+import Badge from '@/components/badge/Badge.vue'
 import Skeleton from '@/components/skeleton/Skeleton.vue'
 </script>
 
@@ -18,6 +20,12 @@ import Skeleton from '@/components/skeleton/Skeleton.vue'
 		<div class="flex gap-4">
 			<div class="bg-danger-100 h-64">asd</div>
 			<Button rounded variant="success">Shadcn Button</Button>
+		</div>
+		<div class="flex items-center gap-2">
+			<Badge type="primary" size="small" closeable>Primary</Badge>
+			<Badge type="danger" size="medium" closeable>Danger</Badge>
+			<Badge type="warning" size="large" closeable>Warning</Badge>
+			<Badge type="purple" size="large" closeable>purple</Badge>
 		</div>
 		<HelloWorld msg="Vite + Vue" />
 		<Skeleton class="h-[125px] w-[250px] rounded-none" />
