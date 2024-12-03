7c023f830bbe035ce86cbd625ca20a555e99498f
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-03 11:28:37 +0700

    Feat/Cu-86cx39hr8__shadcn-checkbox (#14)
    * feat(checkbox): :sparkles: added checkbox component
    
    * fix(checkbox): :bug: fixed if modelvalue type is boolean
    
    * fix(checkbox): :bug: fixed linter
    
    * feat(checkbox): :sparkles: added checkbox validator
    
    * test(checkbox): :white_check_mark: added custom validation test
    
    * feat(checkbox): :sparkles: export into mainjs
    
    * docs(checkbox): :memo: added checkbox component comments
    
    * feat(checkbox): :sparkles: added checkbox app

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,4 @@
 export { Button as SButton } from './components/button';
 export { Input as SInput } from './components/input';
+export { Checkbox as SCheckbox } from './components/checkbox';
+export { CheckboxGroup as SCheckboxGroup } from './components/checkbox';
