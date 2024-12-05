f053c806965c28a2c69a38247fb1195df4499e42
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-05 14:14:07 +0700

    feat(tooltip): :sparkles: added tooltip

---
+++ lib/components/tooltip/Tooltip.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+	TooltipRoot,
+	type TooltipRootEmits,
+	type TooltipRootProps,
+	useForwardPropsEmits,
+	TooltipProvider,
+	TooltipTrigger,
+} from 'radix-vue'
+
+const props = defineProps<TooltipRootProps>()
+const emits = defineEmits<TooltipRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+	<TooltipProvider>
+		<TooltipRoot v-bind="forwarded">
+			<TooltipTrigger as="div" class="w-fit">
+				<slot name="trigger" />
+			</TooltipTrigger>
+			<slot />
+		</TooltipRoot>
+	</TooltipProvider>
+</template>
---
+++ lib/components/tooltip/TooltipContent.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { cn } from '../../utils/tw-merge'
+import {
+	TooltipContent,
+	type TooltipContentEmits,
+	type TooltipContentProps,
+	TooltipPortal,
+	useForwardPropsEmits,
+	TooltipArrow,
+} from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
+import {
+	tooltipVariant,
+	type TooltipVariant,
+	tooltipArrowVariant,
+	type TooltipContentPosition,
+} from '.'
+
+defineOptions({
+	inheritAttrs: false,
+})
+
+const props = withDefaults(
+	defineProps<
+		TooltipContentProps & {
+			class?: HTMLAttributes['class']
+			variant?: TooltipVariant['variant']
+			position?: TooltipContentPosition
+		}
+	>(),
+	{
+		sideOffset: 4,
+	}
+)
+
+const emits = defineEmits<TooltipContentEmits>()
+
+const delegatedProps = computed(() => {
+	const { class: _, ...delegated } = props
+
+	return delegated
+})
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+	<TooltipPortal>
+		<TooltipContent
+			v-bind="{ ...forwarded, ...$attrs }"
+			:class="cn(props.class, tooltipVariant({ variant }))"
+			:side="props.position"
+		>
+			<TooltipArrow :class="cn(tooltipArrowVariant({ variant }))" />
+			<slot />
+		</TooltipContent>
+	</TooltipPortal>
+</template>
