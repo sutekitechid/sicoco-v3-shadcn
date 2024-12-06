a211e8d8ebffadd8e06cd3645cf9805af725c851
maulanairfanf <maulanairfanf@gmail.com>
2024-12-06 17:04:39 +0700

    feat(popover): :triangular_flag_on_post: CU-86cx4f029 change function upsert array to toggle array

--- lib/utils/array.ts
+++ lib/utils/array.ts
@@ -1,25 +1,13 @@
-export const upsertArray = (array, element) => {
-	if (Array.isArray(element) && element.length > 0) {
-		element.forEach(item => {
-			if (array.includes(item)) {
-				array = array.filter(e => e !== item) // Remove existing item
-			} else {
-				array.push(item)
-			}
-		})
-	} else {
-		if (!array) array = []
-		const elIndex = array.indexOf(element)
-		if (elIndex > -1) {
-			// remove item using slice
-			array.splice(elIndex, 1)
-		} else {
-			array.push(element)
-		}
-	}
-	return array
-}
+export function toggleArrayValue<T>(array: T[], value: T): T[] {
+	const index = array.findIndex(
+		item => JSON.stringify(item) === JSON.stringify(value)
+	)
 
-export const isFilledArray = array => {
-	return Array.isArray(array) && array.length > 0
+	if (index !== -1) {
+		array.splice(index, 1)
+	} else {
+		array.push(value)
+	}
+
+	return array
 }
