97884e0e6068e0fd0d3c893142616a9a43d7d85f
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-10 07:33:14 +0700

    feat(upload): :sparkles: added uploader into main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,5 @@ export { Button as SButton } from './components/button';
 export { Input as SInput } from './components/input';
 export { Checkbox as SCheckbox } from './components/checkbox';
 export { CheckboxGroup as SCheckboxGroup } from './components/checkbox';
+export { Upload as SUpload } from './components/upload';
+export { UploadFileIcon as SUploadFileIcon } from './components/upload';
