31e32ac1dded94fdaa4f4ceac397305b62d1b56b
maulanairfanf <maulanairfanf@gmail.com>
2024-12-10 10:04:28 +0700

    feat(accordion): :triangular_flag_on_post: CU-86cx39n8w init accordion

--- src/App.vue
+++ src/App.vue
@@ -145,6 +145,24 @@ const switchModel = ref([
 	},
 ])
 const switchDisable = ref(false)
+const accordionItems = [
+	{
+		value: 'item-1',
+		title: 'Is it accessible?',
+		content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+	},
+	{
+		value: 'item-2',
+		title: 'Is it unstyled?',
+		content:
+			"Yes. It's unstyled by default, giving you freedom over the look and feel.",
+	},
+	{
+		value: 'item-3',
+		title: 'Can it be animated?',
+		content: 'Yes! You can use the transition prop to configure the animation.',
+	},
+]
 const checkboxOptions = [
 	{ label: 'Option 1', value: 'option1' },
 	{ label: 'Option 2', value: 'option2' },
