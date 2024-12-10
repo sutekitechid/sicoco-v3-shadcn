0fcc257cb7fca680adfca6035777c0681116cebe
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-10 07:52:33 +0700

    feat(tooltip): :sparkles: added tooltip into main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,5 @@ export { Button as SButton } from './components/button';
 export { Input as SInput } from './components/input';
 export { Checkbox as SCheckbox } from './components/checkbox';
 export { CheckboxGroup as SCheckboxGroup } from './components/checkbox';
+export { Tooltip as STooltip } from './components/tooltip';
+export { TooltipContent as STooltipContent } from './components/tooltip';
