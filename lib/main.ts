52bf231df04e16154d30ac412d73eb0a7468ad4e
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-10 09:30:23 +0700

    Feat/Cu-86cx39h22__shadcn-modal (#18)
    * feat(dialog): :sparkles: added dialog
    
    * docs(dialog): :memo: added dialog comments for documentation

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,5 @@ export { Button as SButton } from './components/button';
 export { Input as SInput } from './components/input';
 export { Checkbox as SCheckbox } from './components/checkbox';
 export { CheckboxGroup as SCheckboxGroup } from './components/checkbox';
+export { Dialog as SDialog } from './components/dialog';
+export { DialogContent as SDialogContent } from './components/dialog';
