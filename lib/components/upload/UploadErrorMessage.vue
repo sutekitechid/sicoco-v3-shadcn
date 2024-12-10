d329f8977d861ca66403b1a995514bc0e34f3350
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-10 13:34:42 +0700

    Feat/Cu-86cx8km3g__shadcn-upload (#24)
    * feat(upload): :sparkles: added upload component
    
    * feat(upload): :sparkles: added upload component
    
    * feat(upload): :sparkles: added upload default slot
    
    * docs(upload): :memo: added upload component documentation
    
    * feat(upload): :sparkles: added uploader into main.ts
    
    * docs(upload): :memo: added upload documentation
    
    * Merge branch 'main' of https://github.com/sutekitechid/sicoco-v3-shadcn into feat/Cu-86cx8km3g__shadcn-upload

---
+++ lib/components/upload/UploadErrorMessage.vue
@@ -0,0 +1,42 @@
+<template>
+	<BaseInputErrorMessage :invalid="validation.$invalid" class="text-sm">
+		<div v-if="validation.required?.$invalid">
+			<slot name="required" />
+		</div>
+		<div v-else-if="validation.maxSize?.$invalid">
+			<slot name="maxSize" />
+		</div>
+		<div v-else-if="validation.$invalid">
+			<slot name="errors" />
+		</div>
+	</BaseInputErrorMessage>
+</template>
+
+<script setup lang="ts">
+/**
+ * Component that handles the input validation error messages
+ *
+ * @props validation - The validation object
+ *
+ * @slot required - Slot for the required error message
+ *
+ * @example
+ * <UploadErrorMessage :validation="validation">
+ * 	<template #required>
+ * 		This field is required
+ * 	</template>
+ * <template #errors>
+ * 	Invalid file
+ * </template>
+ * </UploadErrorMessage>
+ */
+import BaseInputErrorMessage from '../base-input-error-message'
+
+defineProps<{
+	validation: any
+}>()
+
+const slots = defineSlots<{
+	required?: string
+}>()
+</script>
