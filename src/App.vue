67fe3339048af49da84dead0fece096ad7c37dbe
daulJsx <mrdaul09@gmail.com>
2024-12-11 14:41:47 +0700

    fix(Textarea): :bug: adjustments with main branch

--- src/App.vue
+++ src/App.vue
@@ -479,11 +479,9 @@ const inputDisabledValue = ref('')
   will-change: filter;
   transition: filter 300ms;
 }
-
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
-
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
