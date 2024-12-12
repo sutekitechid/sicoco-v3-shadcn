d33108d73dc65f01b73f8581e867523f6b974af5
Maul <50138748+maulanairfanf@users.noreply.github.com>
2024-12-12 10:52:43 +0700

    feat(badge): :memo: CU-86cx39my9 update documentation and change type to variant (#33)
    * feat(badge): :memo: CU-86cx39my9 update documentation and change type to variant
    
    * test(badge): :white_check_mark: CU-86cx39my9 update unit test

--- src/App.vue
+++ src/App.vue
@@ -205,7 +205,6 @@ const dialogOpened = ref(false)
 			<Badge variant="primary" size="small" closeable>Primary</Badge>
 			<Badge variant="danger" size="medium" closeable>Danger</Badge>
 			<Badge variant="warning" size="large" closeable>Warning</Badge>
-			<Badge variant="purple" size="large" closeable>purple</Badge>
 		</div>
 		<HelloWorld msg="Vite + Vue" />
 		<Skeleton class="h-[125px] w-[250px] rounded-none" />
