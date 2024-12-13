c4efb45b9a1425bee264844e179b3e7a81a2645e
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-13 16:01:44 +0700

    feat: :sparkles: added comment parser

---
+++ lib/components/test.vue
@@ -0,0 +1,43 @@
+<template>Hello World</template>
+
+<script lang="ts">
+/**
+ * Test component is used to test the component.
+ * @example
+ * <Test title="Test Title" description="Test Description" />
+ */
+export default {
+	props: {
+		/**
+		 * Title of the test.
+		 */
+		title: {
+			type: String,
+			required: true,
+		},
+		/**
+		 * Description of the test.
+		 */
+		description: {
+			type: String,
+			required: true,
+		},
+	},
+	setup() {
+		const counter = (ref: Ref<number>) => {
+			ref.value += 1
+		}
+		return {
+			counter,
+		}
+	},
+	methods: {
+		/**
+		 * Method to increment the counter.
+		 */
+		incrementCounter() {
+			this.counter += 1
+		},
+	},
+}
+</script>
