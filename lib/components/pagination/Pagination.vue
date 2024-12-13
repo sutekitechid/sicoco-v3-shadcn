5995908ef4a478d7f3225d35df31e0d77998a065
Dika Rizky Fawzi <dikarfawzi@gmail.com>
2024-12-13 16:18:30 +0700

    refactor(pagination): :recycle: use useVModel, and made some function and computed properties to abstract some process

--- lib/components/pagination/Pagination.vue
+++ lib/components/pagination/Pagination.vue
@@ -21,6 +21,7 @@
  * ```
  */
 import cloneDeep from 'lodash/cloneDeep'
+import { useVModel } from '@vueuse/core'
 import {
 	ref,
 	watch,
@@ -62,20 +63,10 @@ const emit = defineEmits(['update:perPage', 'update:page'])
  * Computed property for perPage that returns the perPage
  * and emits the `update:perPage` event
  */
-const computedPerPage = computed({
-	get: () => props.perPage,
-	set: (value: number) => {
-		emit('update:perPage', value)
-	},
-})
+const computedPerPage = useVModel(props, 'perPage', emit)
 
 /** Computed property for page that returns the page and emits the `update:page` event */
-const computedPage = computed({
-	get: () => props.page,
-	set: (value: number) => {
-		emit('update:page', value)
-	},
-})
+const computedPage = useVModel(props, 'page', emit)
 
 /**
  * Checks if the given page is the active page
@@ -92,6 +83,7 @@ const pageIndex = ref<number>()
 /**
  * Handles input for pagination forward
  * @param value - New page value
+ * @returns void
  */
 function onInputPaginationForward(value: any): void {
 	computedPage.value = cloneDeep(Number(value))
@@ -101,11 +93,70 @@ function onInputPaginationForward(value: any): void {
 watch(computedPerPage, (): void => {
 	computedPage.value = 1
 })
+
+/**
+ * Handles click event for pagination list item
+ * Sets the page number to the clicked page number
+ * @param value - Page number to set
+ * @returns void
+ */
+function onClickPaginationListItem(value: number): void {
+	computedPage.value = value
+}
+
+/**
+ * Handles click event for pagination prev
+ * Decrements the page by 1
+ * @returns void
+ */
+function onClickPaginationPrev(): void {
+	computedPage.value = Number(computedPage) - 1
+}
+
+/**
+ * Handles click event for pagination next
+ * Increments the page by 1
+ * @returns void
+ */
+function onClickPaginationNext(): void {
+	computedPage.value = Number(computedPage) + 1
+}
+
+/**
+ * Checks if the pagination prev button is disabled
+ * @returns true if the pagination prev button is disabled, false otherwise
+ */
+const paginationPrevIsDisabled = computed(() => Number(computedPage) === 1)
+
+/**
+ * Check if the pagination next button is disabled
+ * @returns true if the pagination next button is disabled, false otherwise
+ */
+const paginationNextIsDisabled = computed(
+	() => Number(computedPage) === pageCount.value
+)
+
+/**
+ * Check if the pagination forward is disabled
+ * @returns true if the pagination forward is disabled, false otherwise
+ */
+const paginationForwarIsDisabled = computed(() => {
+	return pageCount.value === 1
+})
+
+/**
+ * Total number of pages
+ * @returns number
+ */
+const pageCount = computed(() => {
+	const total = Number(props.total)
+	const perPage = Number(computedPerPage)
+	return Math.ceil(total / perPage)
+})
 </script>
 
 <template>
 	<Pagination
-		v-slot="{ pageCount }"
 		:page="Number(computedPage)"
 		:total="Number(total)"
 		:sibling-count="1"
@@ -130,26 +181,26 @@ watch(computedPerPage, (): void => {
 						class="w-10 h-10 p-0"
 						variant="primary"
 						:outlined="!isActivePage(item.value)"
-						@click="computedPage = item.value"
+						@click="onClickPaginationListItem(item.value)"
 					>
 						{{ item.value }}
 					</Button>
 				</PaginationListItem>
 			</template>
 			<PaginationPrev
-				@click="computedPage = Number(computedPage) - 1"
-				:disabled="computedPage === 1"
 				class="pagination-prev"
+				@click="onClickPaginationPrev"
+				:disabled="paginationPrevIsDisabled"
 			/>
 			<PaginationNext
-				@click="computedPage = Number(computedPage) + 1"
-				:disabled="computedPage === pageCount"
+				@click="onClickPaginationNext"
+				:disabled="paginationNextIsDisabled"
 				class="pagination-next"
 			/>
 			<PaginationForward
 				class="ml-2 pl-3 border-l-1 border-gray-30"
 				v-model="pageIndex"
-				:disabled="pageCount === 1"
+				:disabled="paginationForwarIsDisabled"
 				@input="onInputPaginationForward"
 			/>
 		</PaginationList>
