dfaa49590a4a8fc34c4df3a2abec6bff1155fc97
daulJsx <mrdaul09@gmail.com>
2024-12-17 08:19:38 +0700

    feat(NavigationMenu): :twisted_rightwards_arrows: match with main

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,15 @@ import { RadioGroupItem, RadioGroup } from '../lib/components/radio'
 import Upload from '@/components/upload/Upload.vue'
 import { Tooltip, TooltipContent } from '../lib/components/tooltip'
 import { Dialog, DialogContent } from '@/components/dialog'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/card/index'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/tabs'
 import NavigationMenu from '@/components/navigation-menu/NavigationMenu.vue'
 import NavItem from '@/components/navigation-menu/NavItem.vue'
 
@@ -710,6 +719,51 @@ const tabsConfig = ref<tasbConfigInterface[]>([
       </DialogContent>
     </Dialog>
     <Button @click="dialogOpened = true">Open Dialog</Button>
+  </div>
+
+  <div class="bg-white p-4">
+    <div v-for="(tabConfig, index) in tabsConfig" :key="index">
+      <Tabs
+        :default-value="tabConfig.defaultValue"
+        :variant="tabConfig.variant"
+      >
+        <TabsList>
+          <TabsTrigger
+            v-for="(trigger, idx) in tabConfig.triggers"
+            :key="idx"
+            :value="trigger.value"
+            :badge-count="trigger.badgeCount"
+          >
+            {{ trigger.label }}
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent
+          v-for="(content, idx) in tabConfig.contents"
+          :key="idx"
+          :value="content.value"
+          class="text-black"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>{{ content.text }}</CardTitle>
+              <CardDescription
+                >Deploy your new project in one-click.</CardDescription
+              >
+            </CardHeader>
+            <CardContent>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate illum repellat et ipsam voluptatum aliquam aspernatur
+              nostrum impedit dolores repudiandae, alias praesentium laudantium
+              corporis eveniet eius consectetur nemo harum! Accusamus.
+            </CardContent>
+            <CardFooter class="flex justify-between px-6 pb-6">
+              <Button> Cancel </Button>
+              <Button>Deploy</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   </div>
 </template>
 
