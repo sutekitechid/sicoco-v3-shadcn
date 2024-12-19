d1b44c88d82fd939d500ab57a035f08d5b5113cd
maulanairfanf <maulanairfanf@gmail.com>
2024-12-19 08:47:51 +0700

    feat(global-style): :lipstick: CU-86cx39dwg add utilities for text stroke

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,42 @@
-const animate = require("tailwindcss-animate")
-const presets = require("./lib/config/configPreset")
+const animate = require('tailwindcss-animate')
+const presets = require('./lib/config/configPreset')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['selector'],
-  content: [
-    './pages/**/*.{ts,tsx,vue}',
-    './src/**/*.{ts,tsx,vue}',
-    './lib/**/*.{ts,tsx,vue}',
+	darkMode: ['selector'],
+	content: [
+		'./pages/**/*.{ts,tsx,vue}',
+		'./src/**/*.{ts,tsx,vue}',
+		'./lib/**/*.{ts,tsx,vue}',
 	],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    }
-  },
-  plugins: [animate],
-  presets: [presets],
+	prefix: '',
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+	},
+	plugins: [
+		animate,
+		function ({ addUtilities }) {
+			const textStrokeUtilities = {
+				'.text-stroke-0': { '-webkit-text-stroke': '0px' },
+				'.text-stroke-0-5': { '-webkit-text-stroke': '0.5px' },
+				'.text-stroke-1': { '-webkit-text-stroke': '1px' },
+				'.text-stroke-1-5': { '-webkit-text-stroke': '1.5px' },
+				'.text-stroke-2': { '-webkit-text-stroke': '2px' },
+				'.text-stroke-2-5': { '-webkit-text-stroke': '2.5px' },
+				'.text-stroke-3': { '-webkit-text-stroke': '3px' },
+				'.text-stroke-3-5': { '-webkit-text-stroke': '3.5px' },
+				'.text-stroke-4': { '-webkit-text-stroke': '4px' },
+				'.text-stroke-4-5': { '-webkit-text-stroke': '4.5px' },
+				'.text-stroke-5': { '-webkit-text-stroke': '5px' },
+			}
+			addUtilities(textStrokeUtilities, ['responsive'])
+		},
+	],
+	presets: [presets],
 }
