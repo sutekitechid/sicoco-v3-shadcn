f6c47b9afbd8e19a2a3bb9c3dfdeafbd9836617d
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-19 08:14:32 +0700

    feat(themes): :sparkles: added change theme (#30)
    * feat(themes): :sparkles: added change theme
    
    * fix(popover): :bug: fixed dropdown background
    
    * feat(toast): :bug: fixed dark mode style
    
    * feat(themes): :sparkles: separated dark mode and theme color
    
    * fix(themes): :lipstick: fixed disabled style
    
    * test(popover): :bug: fixed dropdown test
    
    * Merge branch 'main' of https://github.com/sutekitechid/sicoco-v3-shadcn into feat/handle-multiple-themes

--- lib/components/toast/ToastAction.vue
+++ lib/components/toast/ToastAction.vue
@@ -19,7 +19,7 @@ const delegatedProps = computed(() => {
 		v-bind="delegatedProps"
 		:class="
 			cn(
-				'inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium ring-offset-white transition-colors hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+				'inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium ring-offset-neutral-10 transition-colors hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
 				props.class
 			)
 		"
