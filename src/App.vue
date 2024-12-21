df8640d102b9b47827d5802907d232e20e904036
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-21 21:13:58 +0700

    fix: :bug: fixed app.vue

--- src/App.vue
+++ src/App.vue
@@ -144,37 +144,37 @@ const switchModel = ref([
 		model: true,
 		disabled: false,
 		label: 'Primary',
-		variant: 'primary',
+		variant: 'primary' as 'primary',
 	},
 	{
 		model: true,
 		disabled: false,
 		label: 'Success',
-		variant: 'success',
+		variant: 'success' as 'success',
 	},
 	{
 		model: true,
 		disabled: false,
 		label: 'Warning',
-		variant: 'warning',
+		variant: 'warning' as 'warning',
 	},
 	{
 		model: true,
 		disabled: false,
 		label: 'Danger',
-		variant: 'danger',
+		variant: 'danger' as 'danger',
 	},
 	{
 		model: true,
 		disabled: false,
 		label: 'Secondary',
-		variant: 'secondary',
+		variant: 'secondary' as 'secondary',
 	},
 	{
 		model: true,
 		disabled: false,
 		label: 'Grey / Gray',
-		variant: 'grey',
+		variant: 'grey' as 'grey',
 	},
 ])
 const switchDisable = ref(false)
@@ -768,7 +768,7 @@ const otherNavDropdown = ref([
 						<Switch
 							v-model="item.model"
 							:disabled="item.disabled"
-							:variant="item.variant as 'default' | 'primary' | 'danger' | 'warning' | 'success' | 'secondary' | 'grey' | 'gray' | 'purple'"
+							:variant="item.variant"
 						>
 							<span class="text-neutral-100">
 								{{ item.label }}: {{ item.model }}
@@ -780,85 +780,26 @@ const otherNavDropdown = ref([
 					</span>
 				</div>
 			</div>
-			<div>
-				<span class="text-black">
-					{{ modelDropdownMultiple }}
-				</span>
-				<Dropdown
-					v-model="modelDropdownMultiple"
-					@typing="onSearch"
-					searchable
-					multiple
-				>
-					<DropdownItem
-						v-for="(item, index) in optionDropdown"
-						:key="index"
-						:value="item"
-					>
-						<span class="flex items-center gap-2">
-							{{ item.label }}
-						</span>
-					</DropdownItem>
-				</Dropdown>
-			</div>
-			<div class="flex gap-2">
-				<div v-for="(item, index) in switchModel" :key="index" class="mb-4">
-					<Switch
-						v-model="item.model"
-						:disabled="item.disabled"
-						:variant="item.variant as 'default' | 'primary' | 'danger' | 'warning' | 'success' | 'secondary' | 'grey' | 'gray' | 'purple'"
-					>
-						<span class="text-black"> {{ item.label }}: {{ item.model }} </span>
-					</Switch>
-				</div>
-				<span class="text-black"
-					>disabled <Switch v-model="switchDisable" :disabled="true" />
-				</span>
-			</div>
-		</div>
-		<Input placeholder="Enter your name" size="lg" required>
-			<template #required>
-				<p>Required</p>
-			</template>
-		</Input>
+			<Input placeholder="Enter your name" size="lg" />
 
-		<span class="text-neutral-100 flex">
-			<Checkbox /> checkboxmaul {{ checkboxmaul }}
-		</span>
+			<span class="text-neutral-100 flex">
+				<Checkbox /> checkboxmaul {{ checkboxmaul }}
+			</span>
 
-		<Checkbox
-			ref="checkboxRef"
-			:model-value="selectedOptions.length > 0"
-			:indeterminate="
-				selectedOptions.length > 0 &&
-				selectedOptions.length < checkboxOptions.length
-			"
-			:value="true"
-			required
-			disabled
-		>
-			Pilih Semua
-		</Checkbox>
-		<div class="flex flex-col gap-2 m-6">
-			<div v-for="option in checkboxOptions" :key="option.value">
-				<Checkbox
-					v-model="selectedOptions"
-					:label="option.label"
-					:value="option.value"
-					:key="option.value"
-					variant="success"
-					class="items-start"
-				>
-					<p class="font-semibold mb-2">Remember Me!</p>
-					{{ option.label }} Save my login details for next time.
-				</Checkbox>
-			</div>
-		</div>
-		<FormInput>
-			<CheckboxGroup
-				:value="selectedOptions"
-				:custom-validators="{ test: value => value === 'test' }"
+			<Checkbox
+				ref="checkboxRef"
+				:model-value="selectedOptions.length > 0"
+				:indeterminate="
+					selectedOptions.length > 0 &&
+					selectedOptions.length < checkboxOptions.length
+				"
+				:value="true"
+				required
+				disabled
 			>
+				Pilih Semua
+			</Checkbox>
+			<div class="flex flex-col gap-2 m-6">
 				<div v-for="option in checkboxOptions" :key="option.value">
 					<Checkbox
 						v-model="selectedOptions"
@@ -872,237 +813,259 @@ const otherNavDropdown = ref([
 						{{ option.label }} Save my login details for next time.
 					</Checkbox>
 				</div>
-				<template #errors="{ validation }">
-					<p v-if="validation.test.$invalid">Test error</p>
-				</template>
-			</CheckboxGroup>
-			<button type="submit" class="text-neutral-100">Submit</button>
-		</FormInput>
-		<Breadcrumb>
-			<BreadcrumbItem to="#">Home</BreadcrumbItem>
-			<BreadcrumbItem to="https://google.com" target="_blank"
-				>Library</BreadcrumbItem
-			>
-			<BreadcrumbItem to="#">Data 1</BreadcrumbItem>
-			<BreadcrumbItem to="#">Data 2</BreadcrumbItem>
-			<BreadcrumbItem to="#">Data 3</BreadcrumbItem>
-			<BreadcrumbItem to="#">Data 4</BreadcrumbItem>
-			<BreadcrumbItem to="#" disabled>Data 5</BreadcrumbItem>
-		</Breadcrumb>
-		<RadioGroup v-model="selectedRadio">
-			<RadioGroupItem :value="{ id: 1 }">Option 1</RadioGroupItem>
-			<RadioGroupItem value="option2" variant="success" disabled
-				>Option 2</RadioGroupItem
-			>
-			<RadioGroupItem value="option3" variant="danger">Option 3</RadioGroupItem>
-		</RadioGroup>
-		<Toaster />
-		<Button
-			@click="
-				toast({
-					title: 'Hello World',
-					description: 'This is a toast message',
-					variant: 'success',
-					indefinite: true,
-				})
-			"
-			>Show Toast</Button
-		>
-		<FormInput class="p-6">
-			<div class="grid grid-cols-2 gap-4">
-				<Input placeholder="Nama" disabled />
-				<Upload
-					v-model="selectedFiles"
-					:required="true"
-					:max-size="10"
-					label="Lampirkan file"
-					disabled
+			</div>
+			<FormInput>
+				<CheckboxGroup
+					:value="selectedOptions"
+					:custom-validators="{ test: value => value === 'test' }"
 				>
-					<template #required>
-						<p>Required</p>
-					</template>
-					<template #maxSize>
-						<p>Max size</p>
-					</template>
+					<div v-for="option in checkboxOptions" :key="option.value">
+						<Checkbox
+							v-model="selectedOptions"
+							:label="option.label"
+							:value="option.value"
+							:key="option.value"
+							variant="success"
+							class="items-start"
+						>
+							<p class="font-semibold mb-2">Remember Me!</p>
+							{{ option.label }} Save my login details for next time.
+						</Checkbox>
+					</div>
 					<template #errors="{ validation }">
 						<p v-if="validation.test.$invalid">Test error</p>
 					</template>
-				</Upload>
-			</div>
-			<button type="submit" class="text-neutral-100">Submit</button>
-		</FormInput>
-		<Tooltip>
-			<template #trigger>
-				<Button>Hover me</Button>
-			</template>
-			<TooltipContent variant="success" position="right">
-				<div class="w-96">
-					Lorem Ipsum is simply dummy text of the printing and typesetting
-					industry. Lorem Ipsum has been the industry's standard dummy text ever
-					since the 1500s, when an unknown printer took a galley of type and
-					scrambled it to make a type specimen book. It has survived not only
-					five centuries, but also the leap into electronic typesetting,
-					remaining essentially unchanged. It was popularised in the 1960s with
-					the release of Letraset sheets containing Lorem Ipsum passages, and
-					more recently with desktop publishing software like Aldus PageMaker
-					including versions of Lorem Ipsum.
-				</div></TooltipContent
+				</CheckboxGroup>
+				<button type="submit" class="text-neutral-100">Submit</button>
+			</FormInput>
+			<Breadcrumb>
+				<BreadcrumbItem to="#">Home</BreadcrumbItem>
+				<BreadcrumbItem to="https://google.com" target="_blank"
+					>Library</BreadcrumbItem
+				>
+				<BreadcrumbItem to="#">Data 1</BreadcrumbItem>
+				<BreadcrumbItem to="#">Data 2</BreadcrumbItem>
+				<BreadcrumbItem to="#">Data 3</BreadcrumbItem>
+				<BreadcrumbItem to="#">Data 4</BreadcrumbItem>
+				<BreadcrumbItem to="#" disabled>Data 5</BreadcrumbItem>
+			</Breadcrumb>
+			<RadioGroup v-model="selectedRadio">
+				<RadioGroupItem :value="{ id: 1 }">Option 1</RadioGroupItem>
+				<RadioGroupItem value="option2" variant="success" disabled
+					>Option 2</RadioGroupItem
+				>
+				<RadioGroupItem value="option3" variant="danger"
+					>Option 3</RadioGroupItem
+				>
+			</RadioGroup>
+			<Toaster />
+			<Button
+				@click="
+					toast({
+						title: 'Hello World',
+						description: 'This is a toast message',
+						variant: 'success',
+						indefinite: true,
+					})
+				"
+				>Show Toast</Button
 			>
-		</Tooltip>
-		<Dialog v-model:open="dialogOpened">
-			<DialogContent class="text-neutral-100 text-center w-[400px]">
-				<div class="flex flex-col gap-2 justify-center">
-					<div
-						class="rounded-full h-12 w-12 flex items-center justify-center bg-success-100/10 m-auto"
+			<FormInput class="p-6">
+				<div class="grid grid-cols-2 gap-4">
+					<Input placeholder="Nama" disabled />
+					<Upload
+						v-model="selectedFiles"
+						:required="true"
+						:max-size="10"
+						label="Lampirkan file"
+						disabled
 					>
+						<template #required>
+							<p>Required</p>
+						</template>
+						<template #maxSize>
+							<p>Max size</p>
+						</template>
+						<template #errors="{ validation }">
+							<p v-if="validation.test.$invalid">Test error</p>
+						</template>
+					</Upload>
+				</div>
+				<button type="submit" class="text-neutral-100">Submit</button>
+			</FormInput>
+			<Tooltip>
+				<template #trigger>
+					<Button>Hover me</Button>
+				</template>
+				<TooltipContent variant="success" position="right">
+					<div class="w-96">
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. Lorem Ipsum has been the industry's standard dummy text
+						ever since the 1500s, when an unknown printer took a galley of type
+						and scrambled it to make a type specimen book. It has survived not
+						only five centuries, but also the leap into electronic typesetting,
+						remaining essentially unchanged. It was popularised in the 1960s
+						with the release of Letraset sheets containing Lorem Ipsum passages,
+						and more recently with desktop publishing software like Aldus
+						PageMaker including versions of Lorem Ipsum.
+					</div></TooltipContent
+				>
+			</Tooltip>
+			<Dialog v-model:open="dialogOpened">
+				<DialogContent class="text-neutral-100 text-center w-[400px]">
+					<div class="flex flex-col gap-2 justify-center">
 						<div
-							class="rounded-full h-9 w-9 flex items-center justify-center bg-success-100/20"
+							class="rounded-full h-12 w-12 flex items-center justify-center bg-success-100/10 m-auto"
 						>
-							<i class="si-check-circle text-success-100 h-4 w-4"></i>
+							<div
+								class="rounded-full h-9 w-9 flex items-center justify-center bg-success-100/20"
+							>
+								<i class="si-check-circle text-success-100 h-4 w-4"></i>
+							</div>
+						</div>
+						<h2 class="text-xl font-bold">Successfull</h2>
+						<p class="text-neutral-60 text-sm">
+							This blog post has been published. Team members will be able to
+							edit this post and republish changes.
+						</p>
+						<div class="flex gap-4">
+							<Button outlined @click="dialogOpened = false" class="w-full"
+								>Batal</Button
+							>
+							<Button class="w-full">Confirm</Button>
 						</div>
 					</div>
-					<h2 class="text-xl font-bold">Successfull</h2>
-					<p class="text-neutral-60 text-sm">
-						This blog post has been published. Team members will be able to edit
-						this post and republish changes.
-					</p>
-					<div class="flex gap-4">
-						<Button outlined @click="dialogOpened = false" class="w-full"
-							>Batal</Button
-						>
-						<Button class="w-full">Confirm</Button>
-					</div>
-				</div>
-			</DialogContent>
-		</Dialog>
-		<Button @click="dialogOpened = true">Open Dialog</Button>
-		<div class="text-black">
-			<Accordion class="w-full" type="multiple">
-				<AccordionItem
-					v-for="item in accordionItems"
-					:key="item.value"
-					:value="item.value"
-				>
-					<AccordionTrigger>
-						<template #label>
-							{{ item.title }}
-						</template>
-					</AccordionTrigger>
-					<AccordionContent>
-						{{ item.content }}
-					</AccordionContent>
-				</AccordionItem>
-			</Accordion>
-		</div>
-
-		<Button @click="dialogOpened = true" outlined>Open Dialog</Button>
-
-		<div class="bg-white p-4">
-			<div v-for="(tabConfig, index) in tabsConfig" :key="index">
-				<Tabs
-					:default-value="tabConfig.defaultValue"
-					:variant="tabConfig.variant"
-				>
-					<TabsList>
-						<TabsTrigger
-							v-for="(trigger, idx) in tabConfig.triggers"
-							:key="idx"
-							:value="trigger.value"
-							:badge-count="trigger.badgeCount"
-						>
-							{{ trigger.label }}
-						</TabsTrigger>
-					</TabsList>
-					<TabsContent
-						v-for="(content, idx) in tabConfig.contents"
-						:key="idx"
-						:value="content.value"
-						class="text-black"
+				</DialogContent>
+			</Dialog>
+			<Button @click="dialogOpened = true">Open Dialog</Button>
+			<div class="text-black">
+				<Accordion class="w-full" type="multiple">
+					<AccordionItem
+						v-for="item in accordionItems"
+						:key="item.value"
+						:value="item.value"
 					>
-						<Card>
-							<CardHeader>
-								<CardTitle>{{ content.text }}</CardTitle>
-								<CardDescription
-									>Deploy your new project in one-click.</CardDescription
-								>
-							</CardHeader>
-							<CardContent>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Cupiditate illum repellat et ipsam voluptatum aliquam aspernatur
-								nostrum impedit dolores repudiandae, alias praesentium
-								laudantium corporis eveniet eius consectetur nemo harum!
-								Accusamus.
-							</CardContent>
-							<CardFooter class="flex justify-between px-6 pb-6">
-								<Button> Cancel </Button>
-								<Button>Deploy</Button>
-							</CardFooter>
-						</Card>
-					</TabsContent>
-				</Tabs>
+						<AccordionTrigger>
+							<template #label>
+								{{ item.title }}
+							</template>
+						</AccordionTrigger>
+						<AccordionContent>
+							{{ item.content }}
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
 			</div>
-		</div>
 
-		<Button @click="dialogOpened = true">Open Dialog</Button>
+			<Button @click="dialogOpened = true" outlined>Open Dialog</Button>
 
-		<DatePicker
-			placeholder="Pilih tanggal"
-			v-model="selectedDate"
-			:importantDates="importantDates"
-		/>
-		{{ selectedDate }}
-		<DatePicker
-			placeholder="Pilih rentang tanggal"
-			date-range
-			v-model:start="selectedStartDate"
-			v-model:end="selectedEndDate"
-			:importantDates="importantDates"
-			format-date="full"
-		/>
-		{{ selectedStartDate }}
-		{{ selectedEndDate }}
-		<div class="flex flex-col">
-			<div class="flex">
-				<Calendar v-model="selectedDate" :importantDates="importantDates" />
+			<div class="bg-white p-4">
+				<div v-for="(tabConfig, index) in tabsConfig" :key="index">
+					<Tabs
+						:default-value="tabConfig.defaultValue"
+						:variant="tabConfig.variant"
+					>
+						<TabsList>
+							<TabsTrigger
+								v-for="(trigger, idx) in tabConfig.triggers"
+								:key="idx"
+								:value="trigger.value"
+								:badge-count="trigger.badgeCount"
+							>
+								{{ trigger.label }}
+							</TabsTrigger>
+						</TabsList>
+						<TabsContent
+							v-for="(content, idx) in tabConfig.contents"
+							:key="idx"
+							:value="content.value"
+							class="text-black"
+						>
+							<Card>
+								<CardHeader>
+									<CardTitle>{{ content.text }}</CardTitle>
+									<CardDescription
+										>Deploy your new project in one-click.</CardDescription
+									>
+								</CardHeader>
+								<CardContent>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Cupiditate illum repellat et ipsam voluptatum aliquam
+									aspernatur nostrum impedit dolores repudiandae, alias
+									praesentium laudantium corporis eveniet eius consectetur nemo
+									harum! Accusamus.
+								</CardContent>
+								<CardFooter class="flex justify-between px-6 pb-6">
+									<Button> Cancel </Button>
+									<Button>Deploy</Button>
+								</CardFooter>
+							</Card>
+						</TabsContent>
+					</Tabs>
+				</div>
 			</div>
+
+			<Button @click="dialogOpened = true">Open Dialog</Button>
+
+			<DatePicker
+				placeholder="Pilih tanggal"
+				v-model="selectedDate"
+				:importantDates="importantDates"
+			/>
 			{{ selectedDate }}
-		</div>
-		<div class="flex flex-col">
-			<div class="flex">
-				<RangeCalendar
-					v-model="selectedRangeDate"
-					:importantDates="importantDates"
-				/>
+			<DatePicker
+				placeholder="Pilih rentang tanggal"
+				date-range
+				v-model:start="selectedStartDate"
+				v-model:end="selectedEndDate"
+				:importantDates="importantDates"
+				format-date="full"
+			/>
+			{{ selectedStartDate }}
+			{{ selectedEndDate }}
+			<div class="flex flex-col">
+				<div class="flex">
+					<Calendar v-model="selectedDate" :importantDates="importantDates" />
+				</div>
+				{{ selectedDate }}
 			</div>
-			{{ selectedRangeDate }}
-		</div>
-		{{ importantDates }}
-		<span class="text-black"> {{ selectedStartDate }} </span>
-		<Button @click="dialogOpened = true" outlined>Open Dialog</Button>
-		<h1 class="text-neutral-100 my-3">TextArea Example</h1>
+			<div class="flex flex-col">
+				<div class="flex">
+					<RangeCalendar
+						v-model="selectedRangeDate"
+						:importantDates="importantDates"
+					/>
+				</div>
+				{{ selectedRangeDate }}
+			</div>
+			{{ importantDates }}
+			<span class="text-black"> {{ selectedStartDate }} </span>
+			<Button @click="dialogOpened = true" outlined>Open Dialog</Button>
+			<h1 class="text-neutral-100 my-3">TextArea Example</h1>
 
-		<FormInput>
-			<Textarea
-				v-model="inputValue"
-				id="my-textarea"
-				placeholder="Tulis sesuatu..."
-				:required="true"
-				:minlength="5"
-				:rows="4"
-				:cols="50"
-			>
-			</Textarea>
-			<Textarea
-				v-model="inputDisabledValue"
-				id="my-textarea"
-				placeholder="Tulis sesuatu..."
-				:rows="4"
-				:cols="50"
-				:disabled="true"
-			>
-			</Textarea>
-			<Button type="submit">Submit</Button>
-		</FormInput>
+			<FormInput>
+				<Textarea
+					v-model="inputValue"
+					id="my-textarea"
+					placeholder="Tulis sesuatu..."
+					:required="true"
+					:minlength="5"
+					:rows="4"
+					:cols="50"
+				>
+				</Textarea>
+				<Textarea
+					v-model="inputDisabledValue"
+					id="my-textarea"
+					placeholder="Tulis sesuatu..."
+					:rows="4"
+					:cols="50"
+					:disabled="true"
+				>
+				</Textarea>
+				<Button type="submit">Submit</Button>
+			</FormInput>
+		</div>
 	</div>
 	<div class="bg-neutral-10">
 		<Card shadow rounded border>
