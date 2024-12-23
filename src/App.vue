95eda52893a0ab026a0d2a6fd37b4a5b3036d5ec
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-23 16:56:45 +0700

    fix(input): :bug: fixed prefix position (#44)
    * fix(input): :bug: fixed prefix position
    
    * fix(input): :bug: removed magic value

--- src/App.vue
+++ src/App.vue
@@ -780,7 +780,85 @@ const otherNavDropdown = ref([
 					</span>
 				</div>
 			</div>
-			<Input placeholder="Enter your name" size="lg" />
+			<div class="flex flex-col gap-4 mb-8">
+				<div class="flex gap-4">
+					<div class="text-left">
+						<label for="name" class="text-left text-sm font-semibold"
+							>Name</label
+						>
+						<Input placeholder="Enter your name" required>
+							<template #prefix>
+								<i class="si-user"></i>
+							</template>
+							<template #required> Required </template>
+						</Input>
+					</div>
+					<div class="text-left">
+						<label for="name" class="text-left text-sm font-semibold"
+							>Name</label
+						>
+						<Input placeholder="Enter your name" required>
+							<template #prefix>
+								<i class="si-user"></i>
+							</template>
+							<template #required> Required </template>
+						</Input>
+					</div>
+				</div>
+				<div class="text-left">
+					<label for="name" class="text-left text-sm font-semibold">Name</label>
+					<Input placeholder="Enter your name" required>
+						<template #prefix>
+							<i class="si-user"></i>
+						</template>
+						<template #required> Required </template>
+					</Input>
+				</div>
+				<div class="text-left">
+					<label for="name" class="text-left text-sm font-semibold">Name</label>
+					<Input v-model="inputValue" placeholder="Enter your name" required>
+						<template #prefix>
+							<i class="si-user"></i>
+						</template>
+						<template #required>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+							type and scrambled it to make a type specimen book. It has
+							survived not only five centuries, but also the leap into
+							electronic typesetting, remaining essentially unchanged. It was
+							popularised in the 1960s with the release of Letraset sheets
+							containing Lorem Ipsum passages, and more recently with desktop
+							publishing software like Aldus PageMaker including versions of
+							Lorem Ipsum.
+						</template>
+					</Input>
+				</div>
+				<div class="flex gap-4">
+					<div class="text-left">
+						<label for="name" class="text-left text-sm font-semibold"
+							>Name</label
+						>
+						<Input placeholder="Enter your name" required>
+							<template #prefix>
+								<i class="si-user"></i>
+							</template>
+							<template #required> Required </template>
+						</Input>
+					</div>
+					<div class="text-left">
+						<label for="name" class="text-left text-sm font-semibold"
+							>Name</label
+						>
+						<Input placeholder="Enter your name" required>
+							<template #prefix>
+								<i class="si-user"></i>
+							</template>
+							<template #required> Required </template>
+						</Input>
+					</div>
+				</div>
+			</div>
 
 			<span class="text-neutral-100 flex">
 				<Checkbox /> checkboxmaul {{ checkboxmaul }}
