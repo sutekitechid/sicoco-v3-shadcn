6741f8bc7bd5744626abee6d6a03e057cc3fb34f
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-27 13:23:09 +0700

    fix: :bug: fixed class names contain dot

--- lib/config/configPreset.ts
+++ lib/config/configPreset.ts
@@ -1,6 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
-const animate = require('tailwindcss-animate')
 
 module.exports = {
 	theme: {
@@ -128,22 +127,4 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [
-		animate,
-		function ({ addUtilities }) {
-			const textStrokeUtilities = {
-				'.text-stroke-0': { '-webkit-text-stroke': '0px' },
-				'.text-stroke-0.5': { '-webkit-text-stroke': '0.5px' },
-				'.text-stroke-1': { '-webkit-text-stroke': '1px' },
-				'.text-stroke-1.5': { '-webkit-text-stroke': '1.5px' },
-				'.text-stroke-2': { '-webkit-text-stroke': '2px' },
-				'.text-stroke-2.5': { '-webkit-text-stroke': '2.5px' },
-				'.text-stroke-3': { '-webkit-text-stroke': '3px' },
-				'.text-stroke-3.5': { '-webkit-text-stroke': '3.5px' },
-				'.text-stroke-4': { '-webkit-text-stroke': '4px' },
-				'.text-stroke-4.5': { '-webkit-text-stroke': '4.5px' },
-				'.text-stroke-5': { '-webkit-text-stroke': '5px' },
-			}
-			addUtilities(textStrokeUtilities, ['responsive'])
-		},],
 }
