5be7b45817bf93d50ff8492c2a3a3c2c8ec068b5
Anwar Ramadha <anwarramadha22@gmail.com>
2024-12-30 16:00:52 +0700

    fix(input): :bug: fixed input ts error when get invalid errors (#48)

--- src/App.vue
+++ src/App.vue
@@ -857,6 +857,9 @@ const otherNavDropdown = ref([
 								<i class="si-user"></i>
 							</template>
 							<template #required> Required </template>
+							<template #errors="{ validation }">
+								<p v-if="validation.required.$invalid">Required</p>
+							</template>
 						</Input>
 					</div>
 				</div>
@@ -937,6 +940,9 @@ const otherNavDropdown = ref([
 				<RadioGroupItem value="option3" variant="danger"
 					>Option 3</RadioGroupItem
 				>
+				<template #errors="{ validation }">
+					<p v-if="validation.test.$invalid">Test error</p>
+				</template>
 			</RadioGroup>
 			<Toaster />
 			<Button
