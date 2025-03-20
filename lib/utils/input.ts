0ac60734de26210af7e8984e261e537951aac27f
maulanairfanf <maulanairfanf@gmail.com>
2025-03-20 08:07:12 +0700

    feat(pagination): :triangular_flag_on_post: add default debounce duration

--- lib/utils/input.ts
+++ lib/utils/input.ts
@@ -1,8 +1,9 @@
 import { useDebounceFn } from '@vueuse/core'
+import { DEBOUNCE_DURATION } from './constants'
 
 export function debounceInput(
 	func: (...args: any[]) => void,
-	duration: number
+	duration: number = DEBOUNCE_DURATION
 ) {
 	return useDebounceFn(func, duration)
 }
