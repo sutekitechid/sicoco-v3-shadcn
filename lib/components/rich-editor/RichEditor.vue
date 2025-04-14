771b35a903a547a5a8d9841f6113646fef1d2456
superrdapp <mrdaul09@gmail.com>
2025-04-14 09:23:37 +0700

    feat(rich-editor): add quill-image-uploader dependency and update readOnly prop

--- lib/components/rich-editor/RichEditor.vue
+++ lib/components/rich-editor/RichEditor.vue
@@ -4,7 +4,6 @@ import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
 const props = withDefaults(
 	defineProps<{
-		modelValue?: string | number
 		toolbar?: 'full' | 'essential' | 'minimal' | String | Array<string> | Object
 		readOnly?: boolean
 		placeholder?: string
@@ -12,21 +11,16 @@ const props = withDefaults(
 	}>(),
 	{
 		toolbar: 'full',
-		readOnly: true,
+		readOnly: false,
 		placeholder: '',
 	}
 )
-
-const emit = defineEmits(['update:modelValue'])
 </script>
 
 <template>
 	<QuillEditor
-		v-model="props.modelValue"
 		:toolbar="props.toolbar"
 		:readOnly="props.readOnly"
 		:placeholder="props.placeholder"
-		:options="props.options"
-		@input="value => emit('update:modelValue', value)"
 	/>
 </template>
