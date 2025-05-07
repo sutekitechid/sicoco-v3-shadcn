f7021aac2c801d4194c1ab624360fc0243bea12e
Anwar Ramadha <anwarramadha22@gmail.com>
2025-05-07 11:01:48 +0700

    feat(table): :sparkles: handle dynamic header

--- lib/utils/vnode.ts
+++ lib/utils/vnode.ts
@@ -1,3 +1,4 @@
 import { VNode } from 'vue'
 
+// Symbol(v-fgt) is a vnode type which indicates that the vnode is created by v-for loop
 export const isFragment = (vnode: VNode) => vnode.type === Symbol.for('v-fgt')
