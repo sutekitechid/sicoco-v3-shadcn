d974c00903ca4d8834bdc71577502c01a76c0aa1
Anwar Ramadha <anwarramadha22@gmail.com>
2025-05-09 09:52:08 +0700

    fix(input): :bug: fixed input number when empty. fix numeric input

--- src/App.vue
+++ src/App.vue
@@ -72,6 +72,10 @@ const defaultValue = 'item-1'
 
 const decimalValue = ref(0)
 
+watch(decimalValue, value => {
+	console.log('decimalValue: ', value)
+})
+
 const accordionItems = [
 	{
 		value: 'item-1',
@@ -1332,12 +1336,6 @@ const icons = ['si-lock', 'si-lock-alt', 'si-lock-solid', 'si-lock-circle']
 			</Alert>
 		</div>
 	</div>
-	<Input
-		type="number"
-		v-model="decimalValue"
-		decimal
-		:max-fraction-digits="2"
-	/>
 	<Input type="text" v-model="modelDropdownEmpty" :max-length="10" />
 	<Dropdown v-model="modelDropdownEmpty" class="w-full" data-cy="link-dropdown">
 		<DropdownItem value="" key="">
@@ -1660,6 +1658,16 @@ const icons = ['si-lock', 'si-lock-alt', 'si-lock-solid', 'si-lock-circle']
 								<p v-if="validation.test.$invalid">haha error</p>
 							</template>
 						</Dropdown>
+
+						<Input
+							type="numeric"
+							v-model="decimalValue"
+							decimal
+							:max-fraction-digits="2"
+							required
+						>
+							<template #required> wajib diisi </template>
+						</Input>
 						<button type="submit" class="text-neutral-100">Submit ah</button>
 					</FormInput>
 					<div>
