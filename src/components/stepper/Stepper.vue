942adf7c4985ee8c3eb5c1325f88149790b2ae6e
Dika Rizky Fawzi <dikarfawzi@gmail.com>
2025-05-19 15:54:32 +0700

    fix(stepper): :bug: component imports related to stepper

--- src/components/stepper/Stepper.vue
+++ src/components/stepper/Stepper.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import {
-	SStepperDescription,
-	SStepperIndicator,
-	SStepperItem,
-	SStepper,
-	SStepperTitle,
-	SStepperTrigger,
+	StepperDescription,
+	StepperIndicator,
+	StepperItem,
+	Stepper,
+	StepperTitle,
+	StepperTrigger,
 } from '@/components/stepper'
 
 import { ref } from 'vue'
@@ -35,8 +35,8 @@ const step = ref(2)
 </script>
 
 <template>
-	<SStepper v-model="step" data-cy="our-stepper">
-		<SStepperItem
+	<Stepper v-model="step" data-cy="our-stepper">
+		<StepperItem
 			v-for="(item, index) in steps"
 			:key="item.step"
 			:step="item.step"
@@ -44,25 +44,25 @@ const step = ref(2)
 			:data-cy="`our-stepper-item-${index + 1}`"
 		>
 			<template #trigger>
-				<SStepperTrigger :data-cy="`our-stepper-item-trigger-${index + 1}`">
-					<SStepperIndicator>
+				<StepperTrigger :data-cy="`our-stepper-item-trigger-${index + 1}`">
+					<StepperIndicator>
 						{{ index + 1 }}
-					</SStepperIndicator>
-				</SStepperTrigger>
+					</StepperIndicator>
+				</StepperTrigger>
 			</template>
 
 			<template #label>
 				<div
 					class="top-full left-0 w-full group-data-[state=inactive]:text-neutral-100"
 				>
-					<SStepperTitle class="text-left">
+					<StepperTitle class="text-left">
 						{{ item.title }}
-					</SStepperTitle>
-					<SStepperDescription class="text-left">
+					</StepperTitle>
+					<StepperDescription class="text-left">
 						{{ item.description }}
-					</SStepperDescription>
+					</StepperDescription>
 				</div>
 			</template>
-		</SStepperItem>
-	</SStepper>
+		</StepperItem>
+	</Stepper>
 </template>
