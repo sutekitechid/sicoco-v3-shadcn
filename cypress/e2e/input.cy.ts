e9780dfc74a3f90b715ff284eb54552f1ddd20bb
Anwar Ramadha <anwarramadha22@gmail.com>
2025-05-26 09:38:29 +0700

    test(input): :white_check_mark: added beforeEach that will call cy.visit

--- cypress/e2e/input.cy.ts
+++ cypress/e2e/input.cy.ts
@@ -1,8 +1,11 @@
 /// <reference types="cypress" />
 
+beforeEach(() => {
+	cy.visit('http://localhost:5173/input')
+})
+
 describe('Input prevent default', () => {
 	it('Input field should trigger event prevent default if user input more than 1 dots', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get('[data-cy="cypress-numeric-max-fraction-digits"]').type('1.2')
 		cy.get('[data-cy="cypress-numeric-max-fraction-digits"]').should(
 			'have.value',
@@ -26,7 +29,6 @@ describe('Input prevent default', () => {
 	})
 
 	it('Input field should trigger event prevent default if user input more than 2 digits after dots', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get('[data-cy="cypress-numeric-max-fraction-digits"]').type('1.2')
 		cy.get('[data-cy="cypress-numeric-max-fraction-digits"]').should(
 			'have.value',
@@ -48,12 +50,10 @@ describe('Input prevent default', () => {
 describe('Input number with max value', () => {
 	const dataCy = '[data-cy="cypress-numeric-max-value"]'
 	it('Input field should return correct value', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get(dataCy).type('100')
 		cy.get(dataCy).should('have.value', '100')
 	})
 	it('Input field should trigger event prevent default if user input more than max value (added 1 char behind)', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get(dataCy).type('100')
 		cy.get(dataCy).should('have.value', '100')
 		cy.get(dataCy).type('1')
@@ -64,17 +64,14 @@ describe('Input number with max value', () => {
 describe('Input text with max length', () => {
 	const dataCy = '[data-cy="cypress-text-max-length"]'
 	it('Input field should return correct value (numeric input)', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get(dataCy).type('1234567890')
 		cy.get(dataCy).should('have.value', '1234567890')
 	})
 	it('Input field should return correct value (text input)', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get(dataCy).type('12345678as')
 		cy.get(dataCy).should('have.value', '12345678as')
 	})
 	it('Input field should trigger event prevent default if user input more than max length (added 1 char behind)', () => {
-		cy.visit('http://localhost:5173/input')
 		cy.get(dataCy).type('12345678as')
 		cy.get(dataCy).should('have.value', '12345678as')
 		cy.get(dataCy).type('1')
@@ -84,7 +81,6 @@ describe('Input text with max length', () => {
 
 describe('Handle input copy paste on text typed', () => {
 	it('[PASTE] Input field should have correct value after paste', () => {
-		cy.visit('http://localhost:5173/input')
 		const textToPaste = '1234567890'
 		const dataCy = '[data-cy="cypress-text-max-length"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '1234567890')
@@ -100,7 +96,6 @@ describe('Handle input copy paste on text typed', () => {
 
 describe('Handle input copy paste on number typed', () => {
 	it('[PASTE] Input field should trigger event prevent default if user input more than 1 dots', () => {
-		cy.visit('http://localhost:5173/input')
 		let textToPaste = '1.2'
 		const dataCy = '[data-cy="cypress-numeric-max-fraction-digits"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '1.2')
@@ -122,7 +117,6 @@ describe('Handle input copy paste on number typed', () => {
 	})
 
 	it('[PASTE] Input field should trigger event prevent default if user input more than 100', () => {
-		cy.visit('http://localhost:5173/input')
 		let textToPaste = '100'
 		const dataCy = '[data-cy="cypress-numeric-max-value"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '100')
@@ -145,7 +139,6 @@ describe('Handle input copy paste on number typed', () => {
 
 describe('Handle input type currency', () => {
 	it('Input field should return correct value', () => {
-		cy.visit('http://localhost:5173/input')
 		const dataCy = '[data-cy="cypress-currency"]'
 		cy.get(dataCy).type('1000')
 		cy.get(dataCy).should('have.value', '1.000')
@@ -155,7 +148,6 @@ describe('Handle input type currency', () => {
 		cy.get(dataCy).should('have.value', '100.012')
 	})
 	it('Input field should return correct value if user input non number value', () => {
-		cy.visit('http://localhost:5173/input')
 		const dataCy = '[data-cy="cypress-currency"]'
 		cy.get(dataCy).type('1000')
 		cy.get(dataCy).should('have.value', '1.000')
@@ -165,7 +157,6 @@ describe('Handle input type currency', () => {
 		cy.get(dataCy).should('have.value', '1.000')
 	})
 	it('Input field should return correct value if user input exceed max value', () => {
-		cy.visit('http://localhost:5173/input')
 		const dataCy = '[data-cy="cypress-currency"]'
 		cy.get(dataCy).type('1000')
 		cy.get(dataCy).should('have.value', '1.000')
@@ -180,7 +171,6 @@ describe('Handle input type currency', () => {
 
 describe('Handle input type currency copy paste', () => {
 	it('[PASTE] Input field should have correct value after paste', () => {
-		cy.visit('http://localhost:5173/input')
 		const textToPaste = '1000'
 		const dataCy = '[data-cy="cypress-currency"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '1.000')
@@ -194,7 +184,6 @@ describe('Handle input type currency copy paste', () => {
 	})
 
 	it('[PASTE] Input field should trigger event prevent default if user input more than 1000000', () => {
-		cy.visit('http://localhost:5173/input')
 		let textToPaste = '100'
 		const dataCy = '[data-cy="cypress-currency"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '100')
@@ -208,7 +197,6 @@ describe('Handle input type currency copy paste', () => {
 	})
 
 	it('[PASTE] Input field should trigger event prevent default if user input non number value', () => {
-		cy.visit('http://localhost:5173/input')
 		const dataCy = '[data-cy="cypress-currency"]'
 		cy.get(dataCy).type('1000')
 		cy.get(dataCy).should('have.value', '1.000')
@@ -223,7 +211,6 @@ describe('Handle input type currency copy paste', () => {
 
 describe('Handle input type numeric', () => {
 	it('Input field should return correct value', () => {
-		cy.visit('http://localhost:5173/input')
 		const dataCy = '[data-cy="cypress-numeric"]'
 		cy.get(dataCy).type('1000')
 		cy.get(dataCy).should('have.value', '1000')
@@ -233,7 +220,6 @@ describe('Handle input type numeric', () => {
 		cy.get(dataCy).should('have.value', '10001')
 	})
 	it('Input field should return correct value if user input more than 10 digits', () => {
-		cy.visit('http://localhost:5173/input')
 		const dataCy = '[data-cy="cypress-numeric"]'
 		cy.get(dataCy).type('1234567890')
 		cy.get(dataCy).should('have.value', '1234567890')
@@ -244,7 +230,6 @@ describe('Handle input type numeric', () => {
 
 describe('Handle input type numeric copy paste', () => {
 	it('[PASTE] Input field should have correct value after paste', () => {
-		cy.visit('http://localhost:5173/input')
 		const textToPaste = '1000'
 		const dataCy = '[data-cy="cypress-numeric"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '1000')
@@ -257,7 +242,6 @@ describe('Handle input type numeric copy paste', () => {
 		cy.get(dataCy).clear()
 	})
 	it('[PASTE] Input field should trigger event prevent default if user input more than 10 digits', () => {
-		cy.visit('http://localhost:5173/input')
 		let textToPaste = '100'
 		const dataCy = '[data-cy="cypress-numeric"]'
 		checkHandleInputCopyPaste(dataCy, textToPaste, '100')
