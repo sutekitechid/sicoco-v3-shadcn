140a477a08b650db82ba7ff55e323c9da04da4bb
Anwar Ramadha <anwarramadha22@gmail.com>
2025-06-20 13:09:00 +0700

    feat: :sparkles: added animation

--- lib/config/configPreset.ts
+++ lib/config/configPreset.ts
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+	plugins: [require('tailwindcss-animate')],
 	theme: {
 		colors: {
 			...colors,
