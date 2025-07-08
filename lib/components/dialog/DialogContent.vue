9e2684116a01861983fcde5e9113bc6f88aae1e1
superrdapp <mrdaul09@gmail.com>
2025-07-08 16:42:23 +0700

    fix(dialog): adjust scrollbar styles and improve dialog content structure

--- lib/components/dialog/DialogContent.vue
+++ lib/components/dialog/DialogContent.vue
@@ -62,28 +62,30 @@ const preventCloseWhenClickOutside = event => {
 			:style="{ zIndex: props.zIndex }"
 			:class="
 				cn(
-					'fixed left-1/2 top-1/2 grid w-full -translate-x-1/2 -translate-y-1/2 border bg-white dark:bg-neutral-10 p-10 shadow-lg rounded-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] max-h-[90vh] overflow-y-auto',
+					'fixed left-1/2 top-1/2 grid w-full -translate-x-1/2 -translate-y-1/2 border bg-white dark:bg-neutral-10 p-2 shadow-lg rounded-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
 					props.class
 				)
 			"
 			@interact-outside="preventCloseWhenClickOutside"
 		>
 			<DialogTitle />
-			<slot />
+			<div :class="cn('max-h-[90vh] overflow-y-auto p-10')">
+				<slot />
+			</div>
 		</DialogContent>
 	</DialogPortal>
 </template>
 
 <style scoped>
 ::-webkit-scrollbar {
-	@apply rounded-xl w-3;
+	@apply w-2;
 }
 
 ::-webkit-scrollbar-thumb {
-	@apply rounded-xl bg-neutral-30;
+	@apply rounded-xl bg-neutral-50;
 }
 
-::-webkit-scrollbar-thumb:hover {
-	@apply bg-neutral-40;
+::-webkit-scrollbar-track {
+	@apply rounded-xl bg-neutral-10;
 }
 </style>
