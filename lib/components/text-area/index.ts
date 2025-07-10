9c4c2dd79079bff6e0e9ae67eaf6424814252a5a
superrdapp <mrdaul09@gmail.com>
2025-07-10 14:43:25 +0700

    fix(textarea): add placeholder text size for improved readability

--- lib/components/text-area/index.ts
+++ lib/components/text-area/index.ts
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
 export const textAreaVariants = cva(
-	'bg-transparent box-border p-3 w-full rounded-md border border-neutral-30 focus-visible:ring-2 focus-visible:ring-neutral-50/25 text-neutral-100 placeholder:text-neutral-60 focus-visible:outline-none focus-visible:ring-offset-0 disabled:opacity-50 transition-colors focus-visible:transition-shadow',
+	'bg-transparent box-border p-3 w-full rounded-md border border-neutral-30 focus-visible:ring-2 focus-visible:ring-neutral-50/25 text-neutral-100 placeholder:text-neutral-60 placeholder:text-sm focus-visible:outline-none focus-visible:ring-offset-0 disabled:opacity-50 transition-colors focus-visible:transition-shadow',
 	{
 		variants: {
 			disabled: {
