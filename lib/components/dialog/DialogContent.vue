2620ba79216e937fe27f84276bd02fa29b5eb7eb
Daffa Ulhaq <112412781+superrdapp@users.noreply.github.com>
2025-07-10 13:59:10 +0700

    fix(dialog): adjust dialog content height for improved usability (#264)

--- lib/components/dialog/DialogContent.vue
+++ lib/components/dialog/DialogContent.vue
@@ -62,14 +62,14 @@ const preventCloseWhenClickOutside = event => {
 			:style="{ zIndex: props.zIndex }"
 			:class="
 				cn(
-					'fixed left-1/2 top-1/2 grid w-full -translate-x-1/2 -translate-y-1/2 border bg-white dark:bg-neutral-10 px-2 py-8 shadow-lg rounded-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+					'fixed left-1/2 top-1/2 grid w-full -translate-x-1/2 -translate-y-1/2 border bg-white dark:bg-neutral-10 px-2 py-8 shadow-lg rounded-xl duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] overflow-hidden',
 					props.class
 				)
 			"
 			@interact-outside="preventCloseWhenClickOutside"
 		>
 			<DialogTitle />
-			<div :class="cn('max-h-[90vh] overflow-y-auto px-10 py-3')">
+			<div :class="cn('max-h-[80vh] overflow-y-auto px-10 py-3')">
 				<slot />
 			</div>
 		</DialogContent>
