3a461e66e5b27c96e84957e449f5ee04159d7f7f
Daffa Ulhaq <112412781+superrdapp@users.noreply.github.com>
2025-07-17 11:44:34 +0700

    feat(text-area): :sparkles: datacy (#269)
    * feat(text-area): :sparkles: datacy
    
    * fix(text-area): refactor props definition to use withDefaults for better clarity

--- lib/components/text-area/Textarea.vue
+++ lib/components/text-area/Textarea.vue
@@ -42,19 +42,25 @@ import TextareaErrorMessage from './TextareaErrorMessage.vue'
  * @property {number} [maxlength] - Panjang maksimum teks yang diizinkan.
  */
 
-const props = defineProps<{
-	modelValue?: string
-	class?: HTMLAttributes['class']
-	id?: string
-	placeholder?: string
-	disabled?: boolean
-	required?: boolean
-	minlength?: number
-	rows?: number
-	cols?: number
-	customValidators?: Record<string, unknown>
-	maxlength?: number
-}>()
+const props = withDefaults(
+	defineProps<{
+		modelValue?: string
+		class?: HTMLAttributes['class']
+		id?: string
+		placeholder?: string
+		disabled?: boolean
+		required?: boolean
+		minlength?: number
+		rows?: number
+		cols?: number
+		customValidators?: Record<string, unknown>
+		maxlength?: number
+		dataCy?: string
+	}>(),
+	{
+		dataCy: 'textarea',
+	}
+)
 
 /**
  * Emit event yang didukung oleh komponen TextArea.
@@ -152,6 +158,7 @@ const useValidation = computed(() => {
 					:rows="rows"
 					:cols="cols"
 					:maxlength="props.maxlength"
+					:data-cy="props.dataCy"
 					@blur="validate"
 					@input="
 						$emit(
