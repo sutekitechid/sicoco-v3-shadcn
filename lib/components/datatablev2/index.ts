6a82663e619e96da50dc1405de2cabcd582c109b
Anwar Ramadha <anwarramadha22@gmail.com>
2025-07-21 09:06:53 +0700

    feat: exported DataTableGroupColumn

--- lib/components/datatablev2/index.ts
+++ lib/components/datatablev2/index.ts
@@ -57,7 +57,7 @@ export const COLUMN_SIZE = {
 // Export components
 export { default as DataTable } from './DataTable.vue'
 export { default as DataTableColumn } from './DataTableColumn.vue'
-export { default as DataTableGroup } from './DataTableGroupColumn.vue'
+export { default as DataTableGroupColumn } from './DataTableGroupColumn.vue'
 export { default as DataTableDropdownSettings } from './DataTableDropdownSettings.vue'
 export { default as DataTableColumnSizeDropdown } from './DataTableColumnSizeDropdown.vue'
 export { default as DataTableScrollWrapper } from './DataTableScrollWrapper.vue'
