156826386464409308e19e11ddad8fb261682cc6
superrdapp <mrdaul09@gmail.com>
2025-07-22 14:34:14 +0700

    fix(pin-input): add transition effects to pin input variants for improved UX

--- lib/components/pin-input/index.ts
+++ lib/components/pin-input/index.ts
@@ -2,7 +2,7 @@ import { cva } from 'class-variance-authority'
 export { default as PinInput } from './PinInput.vue'
 
 export const pinInputVariants = cva(
-	'w-10 h-10 bg-white rounded-md text-center text-neutral-100 border border-neutral-30 dark:bg-neutral-10 ring-offset-neutral-10 placeholder:text-neutral-60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-50/40 focus-visible:ring-offset-0 focus-visible:border-primary-100/60 disabled:cursor-not-allowed disabled:opacity-50',
+	'w-10 h-10 bg-white rounded-md text-center text-neutral-100 border border-neutral-30 dark:bg-neutral-10 ring-offset-neutral-10 placeholder:text-neutral-60 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-50/40 focus-visible:ring-offset-0 focus-visible:border-primary-100/60 disabled:cursor-not-allowed disabled:opacity-50 transition-colors focus-visible:transition-shadow',
 	{
 		variants: {
 			disabled: {
