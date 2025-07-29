7e8c4a44b5a3467559a614173aa2e8edd527678f
Anwar Ramadha <anwarramadha22@gmail.com>
2025-07-29 10:28:52 +0700

    fix: fixed type declaration

--- lib/components/datatablev2/DataTableColumn.vue
+++ lib/components/datatablev2/DataTableColumn.vue
@@ -13,19 +13,19 @@ export default {
       default: 1
     },
     bodyColspan: {
-      type: Number | Function,
+      type: [Number, Function],
       default: 1
     },
     bodyRowspan: {
-      type: Number | Function,
+      type: [Number, Function],
       default: 1
     },
     footerColspan: {
-      type: Number | Function,
+      type: [Number, Function],
       default: 1
     },
     footerRowspan: {
-      type: Number | Function,
+      type: [Number, Function],
       default: 1
     },
     group: {
