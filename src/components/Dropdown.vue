460d2d3639ee6a6aa655a28e0eb32990a1014664
Anwar Ramadha <anwarramadha22@gmail.com>
2025-08-01 08:17:12 +0700

    fix(dropdown): :bug: fixed dropdown cannot be opened if there is a pl… (#303)
    * fix(dropdown): :bug: fixed dropdown cannot be opened if there is a placeholder
    
    * fix(dropdown): updated model value checks to handle undefined cases
    
    ---------
    
    Co-authored-by: superrdapp <mrdaul09@gmail.com>

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,51 @@
+<template>
+	<Dropdown
+		v-model="computedModelValue"
+		class="w-full"
+		data-cy="anotherModelDropdownDefaultSelected"
+		placeholder="Select an option"
+	>
+		<DropdownItem :value="undefined">
+			<span>value empty</span>
+		</DropdownItem>
+		<DropdownItem
+			v-for="item in anotherDropdownItems"
+			:key="item.value"
+			:value="item.value"
+		>
+			{{ item.label }}
+		</DropdownItem>
+	</Dropdown>
+</template>
+
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+import Dropdown from '@/components/dropdown/Dropdown.vue'
+import DropdownItem from '@/components/dropdown/DropdownItem.vue'
+import { ref } from 'vue'
+
+const props = defineProps<{
+	modelValue?: string | number | null
+}>()
+
+const emits = defineEmits<{
+	(e: 'update:modelValue', value: string | number | null): void
+}>()
+
+const computedModelValue = useVModel(props, 'modelValue', emits)
+
+const anotherDropdownItems = ref([
+	{
+		label: 'Opsi 1',
+		value: 'option1',
+	},
+	{
+		label: 'Opsi 2',
+		value: 'option2',
+	},
+	{
+		label: 'Opsi 3',
+		value: 'option3',
+	},
+])
+</script>
