e0140227f31c92e09dc7638f68280adf519a8a46
Maul <50138748+maulanairfanf@users.noreply.github.com>
2025-08-04 16:29:05 +0700

    fix(calendar): :lipstick: adjust calendar color by design system (#310)
    * fix(calendar): :lipstick: adjust calendar color by design system
    
    * fix(calendar): :lipstick: change primary 5 to 10

---
+++ src/pages/calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="w-min">
+      <Calendar />
+
+      <RangeCalendar :number-of-months="2" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import Calendar from '../../lib/components/calendar/Calendar.vue'
+import RangeCalendar from '../../lib/components/range-calendar/RangeCalendar.vue'
+</script>
