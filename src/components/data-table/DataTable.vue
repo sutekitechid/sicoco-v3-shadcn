a6c5ca242db4053c40ca510769657a3574db93d9
Anwar Ramadha <anwarramadha22@gmail.com>
2025-08-07 14:40:49 +0700

    fix(table): :bug: fixed test error

--- src/components/data-table/DataTable.vue
+++ src/components/data-table/DataTable.vue
@@ -39,7 +39,7 @@ onMounted(async () => {
 
 const computedData = computed(() => {
 	// This is a placeholder for any computed logic you might want to add
-	const newData = Array.from({ length: 1 }).flatMap(() => data.value)
+	const newData = Array.from({ length: 10000 }).flatMap(() => data.value)
 	return newData.map((item, index) => ({
 		...item,
 		id: `id-${index}`, // Ensure unique IDs for each row
@@ -186,6 +186,7 @@ const cpmkHeaders = ref([
 			show-footer
 			:show-numbering="true"
 			:enable-table-settings="false"
+			:enable-virtual-scroll="true"
 			@sort="$event => console.log('sort', $event)"
 			@change-page="onChangePage"
 			@change-per-page="onChangePerPage"
