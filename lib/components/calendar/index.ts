da73f29493009533f70f2d5615841721a04ea4e3
Maul <50138748+maulanairfanf@users.noreply.github.com>
2025-08-08 10:12:07 +0700

    fix(calendar): :bug: handle calendar years if startyear > end year (#319)
    * fix(calendar): :bug: handle calendar years if startyear > end year
    
    * fix(calendar): :bug: make dropdown years asc
    
    * remove reverse buat mas anwar

--- lib/components/calendar/index.ts
+++ lib/components/calendar/index.ts
@@ -78,7 +78,8 @@ export function getEndYear(yearsRange: number[], currentYear: number) {
 
 export function getYears(startYear: number, endYear: number) {
 	const result = []
-	for (let i = startYear; i <= endYear; i++) {
+
+	for (let i = endYear; i >= startYear; i--) {
 		result.push(i)
 	}
 
