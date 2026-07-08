fce8f9b7040cacdd23ec4b805d681c0ca67cd1d6
Anwar Ramadha <anwarramadha22@gmail.com>
2026-07-08 08:12:46 +0700

    feat: update button size label for clarity

--- src/pages/button.vue
+++ src/pages/button.vue
@@ -5,7 +5,7 @@
 			<div class="flex flex-wrap items-center gap-3">
 				<Button size="sm">Small</Button>
 				<Button size="md">Medium</Button>
-				<Button size="lg">Large</Button>
+				<Button size="lg">Largest button I even seen</Button>
 			</div>
 		</section>
 
