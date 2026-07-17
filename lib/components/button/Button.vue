13a00aec472fe5f21a52085848540d876842464b
Resha Puspita <reshapuspita734@gmail.com>
2026-07-17 15:18:59 +0700

    Feat: cu-86d3q7kkc add button variant tertiary (#446)
    * feat(CU-86d3q7kkc): :sparkles: add new button variant tertiary, duplicate outlined variant
    
    * feat: add icon support for button component with left and right positioning
    
    * feat: update button variant styles and adjust label typography for consistency
    
    ---------
    
    Co-authored-by: anwarramadha <anwarramadha22@gmail.com>

--- lib/components/button/Button.vue
+++ lib/components/button/Button.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, useSlots } from 'vue'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '../../utils/tw-merge'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
-import { type ButtonVariants, buttonVariants } from '.'
+import { type ButtonVariants, buttonVariants, buttonContentVariants } from '.'
 
 interface Props extends PrimitiveProps {
 	variant?: ButtonVariants['variant']
@@ -12,12 +12,16 @@ interface Props extends PrimitiveProps {
 	outlined?: boolean
 	disabled?: boolean
 	to?: string
+	iconLeft?: string
+	iconRight?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
 	as: 'button',
-	to: ''
+	to: '',
 })
+
+const slots = useSlots()
 
 const emits = defineEmits(['click'])
 
@@ -39,6 +43,28 @@ const computedAsChild = computed(() => {
 	if (isRouterLink.value) return true
 	return props.asChild
 })
+
+const hasLeftIcon = computed(() =>
+	Boolean(props.iconLeft || slots['icon-left'])
+)
+
+const hasRightIcon = computed(() =>
+	Boolean(props.iconRight || slots['icon-right'])
+)
+
+const hasText = computed(() => Boolean(slots.default))
+
+const content = computed<NonNullable<ButtonVariants['content']>>(() => {
+	if (!hasText.value && (hasLeftIcon.value || hasRightIcon.value)) {
+		return 'iconOnly'
+	}
+
+	if (hasLeftIcon.value && hasRightIcon.value) return 'iconBoth'
+	if (hasLeftIcon.value) return 'iconLeft'
+	if (hasRightIcon.value) return 'iconRight'
+
+	return 'default'
+})
 </script>
 
 <template>
@@ -47,7 +73,13 @@ const computedAsChild = computed(() => {
 		:as-child="computedAsChild"
 		:class="
 			cn(
-				buttonVariants({ variant, size, outlined, disabled }),
+				buttonVariants({
+					variant,
+					size,
+					outlined,
+					disabled,
+					content,
+				}),
 				props.class
 			)
 		"
@@ -55,11 +87,51 @@ const computedAsChild = computed(() => {
 		@click="onClick"
 	>
 		<RouterLink v-if="isRouterLink" :to="props.to">
-			<div>
+			<div :class="buttonContentVariants({ size })">
+				<template v-if="hasLeftIcon">
+					<span v-if="iconLeft">
+						<i :class="iconLeft" />
+					</span>
+					<span v-else>
+						<slot name="icon-left" />
+					</span>
+				</template>
+
 				<slot />
+
+				<template v-if="hasRightIcon">
+					<span v-if="iconRight">
+						<i :class="iconRight" />
+					</span>
+					<span v-else>
+						<slot name="icon-right" />
+					</span>
+				</template>
 			</div>
 		</RouterLink>
-		<slot v-else />
+		<template v-else>
+			<div :class="buttonContentVariants({ size })">
+				<template v-if="hasLeftIcon">
+					<template v-if="iconLeft">
+						<i :class="iconLeft" />
+					</template>
+					<template v-else>
+						<slot name="icon-left" />
+					</template>
+				</template>
+
+				<slot />
+
+				<template v-if="hasRightIcon">
+					<template v-if="iconRight">
+						<i :class="iconRight" />
+					</template>
+					<template v-else>
+						<slot name="icon-right" />
+					</template>
+				</template>
+			</div>
+		</template>
 	</Primitive>
 </template>
 
