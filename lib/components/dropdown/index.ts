4c1c99e64cfff7a4086656745e2cfd81105ef881
Daffa Ulhaq <112412781+superrdapp@users.noreply.github.com>
2026-09-23 14:08:59 +0700

    fix: show counter based on selected items and style adjusment for dropdown item when disable  (#533)
    * fix: show counter when selected and disabled state dropdown item
    
    * fix: font color for disable state

--- lib/components/dropdown/index.ts
+++ lib/components/dropdown/index.ts
@@ -52,7 +52,7 @@ export const dropdownTriggerVariants = cva(
 	{
 		variants: {
 			disabled: {
-				true: 'bg-disabled text-main cursor-not-allowed hover:text-main',
+				true: 'bg-disabled text-disabled cursor-not-allowed hover:text-main',
 			},
 		},
 	},
@@ -68,7 +68,8 @@ export const dropdownItemVariants = cva(
 		variants: {
 			type: {
 				selected: 'bg-primary-subtle cursor-pointer',
-				disabled: 'bg-disabled cursor-not-allowed',
+				disabled:
+					'bg-transparent text-disabled border-transparent shadow-none cursor-not-allowed hover:bg-transparent active:bg-transparent',
 				default: 'hover:bg-primary-subtle cursor-pointer',
 				'multiple-select': 'hover:bg-primary-subtle cursor-pointer',
 			},
